e6012543a4de9ba68c121a484761c65170bf67dd
鈴木 友数 <tt.suzuki@uchida-it.co.jp>
2023-09-06 08:17:24 +0900

    認証外す

--- angular-app/src/app/core/components/nav.component.ts
+++ angular-app/src/app/core/components/nav.component.ts
@@ -16,7 +16,7 @@ import { UserInfo } from '../model/user-info';
       </ul>
     </nav>
     <nav class="menu auth">
-  <p class="menu-label">Auth</p>
+  <!-- <p class="menu-label">Auth</p>
   <div class="menu-list auth">
     <ng-container *ngIf="!userInfo; else logout">
       <ng-container *ngFor="let provider of providers">
@@ -32,7 +32,7 @@ import { UserInfo } from '../model/user-info';
   <p>Welcome</p>
   <p>{{ userInfo?.userDetails }}</p>
   <p>{{ userInfo?.identityProvider }}</p>
-</div>
+</div> -->
   `,
 })
 export class NavComponent implements OnInit {
